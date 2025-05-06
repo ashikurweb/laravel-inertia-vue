91d825b8b90cf378bba600e98388178d10db4bc5
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-06 23:45:27 +0600

    wip: setup taiwind 4

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1>Hello</h1>
+        <h1 class="text-5xl font-bold text-gray-600">Hello</h1>
     </div>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <title>Laravel Inertia Vue</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/js/app.js')
     @inertiaHead
 </head>
 <body>
