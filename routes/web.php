cd6ac04a0df5775b505daa841ed038da162bd1bc
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-07 11:17:37 +0600

    wip: name route

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Inertia\Inertia;
 Route::get('/', function () {
     sleep(1);
     return Inertia::render('Home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     sleep(1); 
     return Inertia::render('About', [
         'name' => 'Jeffery Way',
     ]);
-});
+})->name('about');
