1a12cd2f06371d6207d153bb89c49cc9e124f44a
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-10 19:09:33 +0600

    wip: issue profile image not uploading

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,6 +25,7 @@ class RegisterRequest extends FormRequest
             'name'      => ['required', 'string', 'max:255'],
             'email'     => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password'  => ['required', 'confirmed', 'min:6'],
+            'image'     => ['nullable', 'image', 'mimes:jpg,jpeg,png,gif', 'max:2048']
         ];
     }
 
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -8,7 +8,12 @@ const form = useForm({
     email: '',
     password: '',
     password_confirmation: '',
+    image: '',
 });
+
+const change = (e) => {
+    form.image = e.target.files[0];
+}
 
 const formSubmit = () => {
     form.post(route('register'));
@@ -22,7 +27,7 @@ const formSubmit = () => {
         <h2 class="text-2xl font-semibold text-gray-800 mb-6 text-center">Create an Account</h2>
   
         <!-- Form Section -->
-        <form @submit.prevent="formSubmit">
+        <form @submit.prevent="formSubmit" enctype="multipart/form-data">
           <!-- Name Field -->
           <TextInput
             v-model="form.name"
@@ -60,6 +65,17 @@ const formSubmit = () => {
             id="password_confirmation"
             placeholder="Confirm your password"
             />
+
+            <div>
+                <label for="image" class="block mb-2 text-base font-semibold text-gray-600">Image</label>
+                <input 
+                  type="file" 
+                  id="image" 
+                  name="image" 
+                  @input="change"
+                  class="w-full px-4 py-2 border text-gray-500 border-slate-300 rounded-md focus:ring-2 focus:outline-none transition-all duration-300"
+                  />
+            </div>
   
           <!-- Submit Button -->
           <button
