27124ee8c8b11bd9ad89ed0d7709aadbc2206757
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-10 11:59:40 +0600

    wip: fixed layout and added dropdown

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <template>
+    <Head title="Dashboard"/>
     <div>
         <h1 class="text-3xl font-bold text-lime-400"><span class="text-slate-700">Welcome back,</span> {{ $page.props.auth.user.name }}</h1>
     </div>
