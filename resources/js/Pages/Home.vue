2ea094da49251b87d548c684d2a9f083d53d0e1b
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-11 17:55:50 +0600

    wip: Pagination single page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Paginator from './Components/Paginator.vue';
+
 defineProps({
     users: Object
 })
@@ -64,31 +66,6 @@ const getDate = (date) =>
         </table>
 
         <!-- Pagination -->
-        <div class="flex items-center justify-between mt-4 px-4 py-3 bg-white border-t border-gray-200 sm:px-6">
-            <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                <div>
-                    <p class="text-sm text-gray-700">
-                        Showing
-                        <span class="font-medium">{{ users.from }}</span>
-                        to
-                        <span class="font-medium">{{ users.to }}</span>
-                        of
-                        <span class="font-medium">{{ users.total }}</span>
-                        results
-                    </p>
-                </div>
-                <div>
-                    <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-                        <a v-for="(link, index) in users.links" :key="index" :href="link.url"
-                            @click.prevent="$inertia.visit(link.url)"
-                            class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                            :class="{
-                                'bg-indigo-50 border-indigo-500 text-indigo-600': link.active,
-                                'pointer-events-none': !link.url
-                            }" v-html="link.label"></a>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <Paginator :links="users.links" />
     </div>
 </template>
